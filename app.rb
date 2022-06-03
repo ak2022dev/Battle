41242e51340cefbe5a883badbdb5d44139ac0bf9
Amir Khan <ak2022dev@gmail.com>
2022-06-03 17:47:57 +0100

    Refactored to post/redirect/get pattern

--- app.rb
+++ app.rb
@@ -13,17 +13,23 @@ class Battle < Sinatra::Base
   end
   # our routes would go here
 =end
-
+  enable :sessions
+  
   get '/' do
     erb(:index) 
   end 
 
   post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
-    erb(:play)
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect 'play'
   end
   
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
+    erb(:play)
+  end    
   # # Start the server if this file is executed directly (do not change the line below)
   run! if app_file == $0
 end
