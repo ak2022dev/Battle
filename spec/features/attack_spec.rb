df2ebd0a6b56c1a18b8cb7e6ed13e40e654315dd
Amir Khan <ak2022dev@gmail.com>
2022-06-04 13:46:00 +0100

    Added support for model but some tests failing

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,9 +1,18 @@
 # in spec/features/attack_spec.rb
 
 feature 'Attacking' do
+
   scenario 'attack Player 2' do
     sign_in_and_play
     click_link 'Attack'
     expect(page).to have_content 'Amir attacked Khan'
+  end
+
+  scenario 'reduce Player 2 HP by 10' do
+    sign_in_and_play
+    click_button 'Attack'
+    click_link 'OK'
+    expect(page).not_to have_content 'Khan: 60HP'
+    expect(page).to have_content 'Khan: 50HP'
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,10 +3,30 @@ require 'player'
 
 describe Player do
   subject(:amir) { Player.new('Amir') }
+  subject(:khan) { Player.new('Khan') }
   
   describe '#name' do
     it 'returns the name' do
       expect(amir.name).to eq 'Amir'
+    end
+  end
+  
+  describe '#hit_points' do
+    it 'returns the hit points' do
+      expect(amir.hit_points).to eq described_class::DEFAULT_HIT_POINTS
+    end
+  end
+  
+  describe '#attack' do
+    it 'damages the player' do
+      expect(khan).to receive(:receive_damage)
+      amir.attack(khan)
+    end
+  end
+
+  describe '#receive_damage' do
+    it 'reduces the player hit points' do
+      expect { amir.receive_damage }.to change { khan.hit_points }.by(-10)
     end
   end
 end
